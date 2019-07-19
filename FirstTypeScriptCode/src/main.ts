2915bae8c0426e53c737159570e3c07bf60cd613
said moutaa <saiddev@saids-MacBook-Air.local>
2019-07-19 18:45:41 +0200

    new update

--- FirstTypeScriptCode/src/main.ts
+++ FirstTypeScriptCode/src/main.ts
@@ -3,18 +3,14 @@ class Point{
     x:number;
     y:number;
 
-    constructor(x:number, y:number){
-        this.x=x;
-        this.y=y;
-    }
-
     draw(){
         console.log("these are the coordinates of the points: X" + this.x + " and Y: " + this.y)
     }
 }
 
 
-let p= new Point(1,3);
-
+let p= new Point();
+p.x=1;
+p.y=2;
 p.draw();
 
