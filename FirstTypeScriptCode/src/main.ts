a631135ed11361b74f127a6f6c3ecf2b546a0f00
said moutaa <saiddev@saids-MacBook-Air.local>
2019-07-19 19:59:23 +0200

    adding modules

--- FirstTypeScriptCode/src/main.ts
+++ FirstTypeScriptCode/src/main.ts
@@ -1,44 +1,7 @@
-class Point{
-
-    constructor(private _x?:number, private _y?:number){
-    }
-
-    draw(){
-        console.log("these are the coordinates of the points: X" + this.x + " and Y: " + this.y)
-    }
-
-    // adding getters and setters
-
-    getX(){
-        return this.x;
-    }
-
-    getY(){
-        return this.y;
-    }
-    
-    setX(value){
-
-        if (value<0){
-            throw new Error ("the value can not be less than 0");
-        }
-        else
-        this.x=value;
-    }
-
-    setY(value){
-
-        if (value<0){
-            throw new Error ("the value can not be less than 0");
-        }
-        else
-        this.x=value;
-    }
-
-}
+import {Point} from "./Point";
 
 let p= new Point(1,3);
-p.getX();
-p.setX(10);
+let x= p.x;
+p.x = 10;
 p.draw();
 
